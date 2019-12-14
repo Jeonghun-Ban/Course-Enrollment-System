d3892d604632e1541b5525e9fe8970be0bb94133
Jeonghun-Ban <bjh5230@gmail.com>
2019-12-15 05:50:32 +0900

    resolution err: login-option

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,12 +3,16 @@ package main;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import javax.swing.JFrame;
 
 import Account.LoginFrame;
 import Account.LoginOption;
 import Cource.CourceFrame;
+import Framework.ICLogin;
+import Framework.Launcher;
 
 public class Main {
 
@@ -21,6 +25,10 @@ public class Main {
 	private static InputStream inputStream = null;
 	private static OutputStream outputStream = null;
 	
+	private static String id;
+
+	private static String pw;
+	
 	public static void main(String[] args) {
 		try {
 			Connector.initialize();
@@ -28,8 +36,33 @@ public class Main {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
+		
 
 		if(option[0].equals("auto")) {
+			
+			id = option[1];
+			pw = option[2];
+			
+			
+			try {
+				// 로그인
+				Class<?> iCLogin = ICLogin.class;
+				Method method = iCLogin.getMethod("authenticate", String.class, String.class);
+				Connector.invoke(new Launcher(iCLogin.getSimpleName(), method.getName(), method.getParameterTypes(), new Object[]{id, pw.toString()}));
+				
+				method = iCLogin.getMethod("getName");
+				CurrentUser.name = (String) Connector.invoke(new Launcher(iCLogin.getSimpleName(), method.getName(), method.getParameterTypes(), new Object[]{}));
+				method = iCLogin.getMethod("getMajor");
+				CurrentUser.major = (String) Connector.invoke(new Launcher(iCLogin.getSimpleName(), method.getName(), method.getParameterTypes(), new Object[]{}));
+				method = iCLogin.getMethod("getCredit");
+				CurrentUser.credit = (int) Connector.invoke(new Launcher(iCLogin.getSimpleName(), method.getName(), method.getParameterTypes(), new Object[]{}));
+				CurrentUser.id = id;
+				
+			} catch (InvocationTargetException | NoSuchMethodException | SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			courceFrame = new CourceFrame();
 			courceFrame.setVisible(true);
 			courceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
