cdd9f28f4a0e7bdcdd0b0de898f4f81dbf3981b3
Jeonghun-Ban <bjh5230@gmail.com>
2019-12-14 17:14:21 +0900

    Modify CurrentUser

--- src/main/CurrentUser.java
+++ src/main/CurrentUser.java
@@ -1,6 +1,9 @@
 package main;
 
 public class CurrentUser {
+	public static String name;
+	public static String id;
+	public static String major;
 	public static int credit = 18;
 	public static int apply;
 }
