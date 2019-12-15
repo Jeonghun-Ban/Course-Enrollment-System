dd9c1bd249f117eee1bbb1eabf52af3376ef79ba
Jeonghun-Ban <bjh5230@gmail.com>
2019-12-15 21:53:25 +0900

    Modify UserInforFrame

--- src/Cource/UserInfoFrame.java
+++ src/Cource/UserInfoFrame.java
@@ -1,7 +1,12 @@
 package Cource;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -20,20 +25,34 @@ public class UserInfoFrame extends JFrame {
 		this.setSize(400, 545);
 		
 		id = new JLabel();
-		id.setText(CurrentUser.id);
+		id.setText("아이디: " + CurrentUser.id);
 		name = new JLabel();
-		name.setText(CurrentUser.name);
+		name.setText("이름: " + CurrentUser.name);
 		major = new JLabel();
-		major.setText(CurrentUser.major);
+		major.setText("전공: " + CurrentUser.major);
 		credit = new JLabel();
-		credit.setText(Integer.toString(CurrentUser.credit));
+		credit.setText("이수가능학점: " + Integer.toString(CurrentUser.credit));
 		
 		this.add(id);
 		this.add(name);
 		this.add(major);
 		this.add(credit);
 		
+		this.setTitle("유저정보");
 		this.setLayout(new FlowLayout());
+
+		// 폰트
+		Font font = new Font("고딕", Font.TRUETYPE_FONT, 15);
+
+		// 아이콘 이미지
+		File icon = new File("image/icon.gif");
+		try {
+			Image img = ImageIO.read(icon);
+			setIconImage(img);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
